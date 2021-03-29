2f93c3d4cae52bb7d6f537cd0d038e137a19ccd1
PrakGov <55034062+prak112@users.noreply.github.com>
2021-03-29 16:25:06 +0300

    just for fun, but working phonebook

---
+++ phonebook.py
@@ -0,0 +1,41 @@
+# TASK
+"""Given  names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers. 
+You will then be given an unknown number of names to query your phone book for. 
+For each  queried, print the associated entry from your phone book on a new line in the form name=phoneNumber; 
+if an entry for  is not found, print Not found instead."""
+
+# Input Format
+"""
+The first line contains an integer, , denoting the number of entries in the phone book.
+Each of the  subsequent lines describes an entry in the form of  space-separated values on a single line. 
+The first value is a friend's name, and the second value is an -digit phone number.
+
+After the  lines of phone book entries, there are an unknown number of lines of queries. 
+Each line (query) contains a  to look up, and you must continue reading lines until there is no more input.
+
+Note: Names consist of lowercase English alphabetic letters and are first names only.
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+        
+    e = [input().split(' ') for _ in range(n)]
+    
+    q = []
+    while True:
+        key = input().split()
+        if len(key) == 0:
+            break
+        else:
+            q.append(key)
+            continue
+
+            
+entries = dict(e)
+for query in q:
+    for name in query:
+        if name in entries:
+            print(f"{name}={entries[name]}")
+        else:
+            print(name,': Not found')
+            continue
